84c89d52adb9066f4b0c0f382c6d67e5078e4324
Ginnu G <156671422+ginnuganesan@users.noreply.github.com>
2024-02-20 19:16:36 +0530

    Create Medium_LongestConsecutiveSubsequence.java
    Problem Solved Successfully
    Test Cases Passed: 
    1118 /1118
    Total Points Scored: 
    4 /4
    Your Total Score: 
    77
    Total Time Taken: 
    0.44
    Your Accuracy: 
    33%
    Attempts No.: 
    3

---
+++ Medium_LongestConsecutiveSubsequence.java
@@ -0,0 +1,78 @@
+/* PROBLEM DESCRIPTION
+
+Given an array of positive integers. Find the length of the longest sub-sequence such that elements in the subsequence are consecutive integers, the consecutive numbers can be in any order.
+ 
+
+Example 1:
+
+Input:
+N = 7
+a[] = {2,6,1,9,4,5,3}
+Output:
+6
+Explanation:
+The consecutive numbers here
+are 1, 2, 3, 4, 5, 6. These 6 
+numbers form the longest consecutive
+subsquence.
+Example 2:
+
+Input:
+N = 7
+a[] = {1,9,3,10,4,20,2}
+Output:
+4
+Explanation:
+1, 2, 3, 4 is the longest
+consecutive subsequence.
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function findLongestConseqSubseq() which takes the array arr[] and the size of the array as inputs and returns the length of the longest subsequence of consecutive integers. 
+
+
+Expected Time Complexity: O(R), where R is the maximum integer in the array.
+Expected Auxiliary Space: O(N).
+
+
+Constraints:
+1 <= N <= 105
+0 <= a[i] <= 105
+
+*/
+
+//Solution
+
+class Solution
+{   
+    // arr[] : the input array
+    // N : size of the array arr[]
+    
+    //Function to return length of longest subsequence of consecutive integers.
+	static int findLongestConseqSubseq(int arr[], int N)
+	{
+	   // add your code here
+	   Arrays.sort(arr);
+	   int min = arr[0];
+	   int max = arr[arr.length - 1];
+	   int maxSeq = 0, j = 0;
+	   int count = 0;
+	   for(int i = min; i <= max; i++) {
+	       if(j < arr.length) {
+	           if(i == arr[j]) {
+    	           count++;
+    	       } else {
+                    if(!(j > 0 && arr[j] == arr[j-1])) {
+                        count = 1;
+                        i = arr[j];
+                    } else {
+                        i--;
+                    }
+    	       }
+    	       maxSeq = Math.max(maxSeq, count);
+    	       j++;
+	       }
+	   }
+	   return maxSeq;
+	   
+	}
+}
