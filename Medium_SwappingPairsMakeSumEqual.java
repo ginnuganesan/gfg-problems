71bc3e98535cb0a8b42af9e4910ff0ab8d9f9709
Ginnu G <156671422+ginnuganesan@users.noreply.github.com>
2024-06-06 19:29:18 +0530

    TLE

---
+++ Medium_SwappingPairsMakeSumEqual.java
@@ -0,0 +1,58 @@
+/* PROBLEM DESCRIPTION
+
+Given two arrays of integers a[] and b[] of size n and m, the task is to check if a pair of values (one value from each array) exists such that swapping the elements of the pair will make the sum of two arrays equal.
+
+Note: Return 1 if there exists any such pair otherwise return -1.
+
+Example 1:
+
+Input: n = 6, m = 4, a[] = {4, 1, 2, 1, 1, 2}, b[] = (3, 6, 3, 3)
+Output: 1
+Explanation: Sum of elements in a[] = 11, Sum of elements in b[] = 15, To get same sum from both arrays, we can swap following values: 1 from a[] and 3 from b[]
+Example 2:
+
+Input: n = 4, m = 4, a[] = {5, 7, 4, 6}, b[] = {1, 2, 3, 8}
+Output: 1
+Explanation: We can swap 6 from array a[] and 2 from array b[]
+Expected Time Complexity: O(mlogm+nlogn).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 ≤ n, m ≤ 106
+0 <= a[i], b[i] <= 105
+
+*/
+
+//Solution
+
+class Solution {
+    long findSwapValues(long a[], int n, long b[], int m) {
+        // Your code goes here
+        long sumA = 0, sumB = 0;
+        for(long num: a) {
+            sumA += num;
+        }
+        for(long num: b) {
+            sumB += num;
+        }
+        long diff = Math.abs(sumA - sumB);
+        if(diff % 2 != 0) {
+            return -1;
+        }
+        int i = 0, j = 0;
+        long target = diff / 2;
+        Arrays.sort(a);
+        Arrays.sort(b);
+        while(i < n && j < m) {
+            long valDiff = Math.abs(a[i] - b[j]);
+            if(valDiff == target) {
+                return 1;
+            } else if (valDiff < target) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        return -1;
+    }
+}
