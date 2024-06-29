ceeffba62a2b0c9cadfaa29190052fa29ea9b9bc
Ginnu G <156671422+ginnuganesan@users.noreply.github.com>
2024-06-29 21:45:30 +0530

    Create Identical Linked Lists.java
    Problem Solved Successfully
    Test Cases Passed
    1115 / 1115
    Attempts : Correct / Total
    1 / 1
    Accuracy :
    100%
    Points Scored 
    1 / 1
    Your Total Score:
    334 
    Time Taken
    1.35

---
+++ Identical Linked Lists.java
@@ -0,0 +1,61 @@
+/* PROBLEM DESCRIPTION
+
+Given the two singly Linked Lists respectively. The task is to check whether two linked lists are identical or not. 
+Two Linked Lists are identical when they have the same data and with the same arrangement too. If both Linked Lists are identical then return true otherwise return false. 
+
+Examples:
+
+Input:
+LinkedList1: 1->2->3->4->5->6
+LinkedList2: 99->59->42->20
+Output: false
+Explanation:
+
+As shown in figure linkedlists are not identical.
+Input:
+LinkedList1: 1->2->3->4->5
+LinkedList2: 1->2->3->4->5
+Output: true
+Explanation: 
+ 
+As shown in figure both are identical.
+Expected Time Complexity: O(n)
+Expected Auxilliary Space: O(1)
+
+Constraints:
+1 <= length of lists <= 103
+
+*/
+
+//Solution
+
+/*
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}*/
+
+class Solution {
+    // Function to check whether two linked lists are identical or not.
+    public boolean areIdentical(Node head1, Node head2) {
+        // write your code here
+        Node temp1 = head1;
+        Node temp2 = head2;
+        while(temp1 != null && temp2 != null) {
+            if(temp1.data != temp2.data) {
+                return false;
+            }
+            temp1 = temp1.next;
+            temp2 = temp2.next;
+        }
+        if(temp1 == null && temp2 == null) {
+            return true;
+        }
+        return false;
+    }
+}
