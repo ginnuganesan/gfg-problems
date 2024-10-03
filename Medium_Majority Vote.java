8b97a576b6d76d90c19ba9dc21c13a285b408c71
Ginnu G <156671422+ginnuganesan@users.noreply.github.com>
2024-10-03 13:34:19 +0530

    Create Medium_Majority Vote.java
    Problem Solved Successfully
    Test Cases Passed
    1112 / 1112
    Attempts : Correct / Total
    1 / 1
    Accuracy :
    100%
    Points Scored 
    4 / 4
    Your Total Score:
    478 
    Time Taken
    1.8

---
+++ Medium_Majority Vote.java
@@ -0,0 +1,46 @@
+/* PROBLEM DESCRIPTION
+
+You are given an array of integer nums[] where each number represents a vote to a candidate. Return the candidates that have votes greater than one-third of the total votes, If there's not a majority vote, return -1. 
+
+Note: The answer should be returned in an increasing format.
+
+Examples:
+
+Input: nums = [2, 1, 5, 5, 5, 5, 6, 6, 6, 6, 6]
+Output: [5, 6]
+Explanation: 5 and 6 occur more n/3 times.
+Input: nums = [1, 2, 3, 4, 5]
+Output: [-1]
+Explanation: no candidate occur more than n/3 times.
+Expected Time Complexity: O(n)
+Expected Space Complexity: O(1)
+
+Constraint:
+1 <= nums.size() <= 106
+0 <= nums[i] <= 109
+
+*/
+
+//Solution
+
+class Solution {
+    // Function to find the majority elements in the array
+    public List<Integer> findMajority(List<Integer> nums) {
+        // Your code goes here.
+        int n = nums.size();
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int num: nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        List<Integer> out = new ArrayList<>();
+        for(Map.Entry<Integer, Integer> entry: map.entrySet()) {
+            if(entry.getValue() > n/3) {
+                out.add(entry.getKey());
+            }
+        }
+        if(out.isEmpty()) {
+            out.add(-1);
+        }
+        return out;
+    }
+}
